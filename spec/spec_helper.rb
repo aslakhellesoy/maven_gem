1afb599e5e48396aa6c62fe076498473ebc63f40
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-10-02 14:03:42 +0200

    Use new RSpec API

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,7 @@
-begin
-  require 'spec'
-rescue LoadError
-  require 'rubygems'
-  gem 'rspec'
-  require 'spec'
-end
-
-$:.unshift(File.dirname(__FILE__) + '/../lib')
-
 require 'maven_gem'
-require 'rubygems'
 require 'mocha'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   config.mock_with :mocha
 end
 
